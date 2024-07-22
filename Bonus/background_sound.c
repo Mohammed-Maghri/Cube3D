50a7edf443624d9a998d113ad41d17d01e9d9707
Chorouk Masnaoui <cmasnaou@e3r4p15.1337.ma>
2024-07-22 09:37:12 +0100

    norm fixed

--- Bonus/background_sound.c
+++ Bonus/background_sound.c
@@ -6,26 +6,27 @@
 /*   By: cmasnaou <cmasnaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 12:34:15 by mmaghri           #+#    #+#             */
-/*   Updated: 2024/07/21 15:49:48 by cmasnaou         ###   ########.fr       */
+/*   Updated: 2024/07/22 09:36:44 by cmasnaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D_bonus.h"
 
-void *thread_function_handler(char *text)
+void	*thread_function_handler(char *text)
 {
-    system(text);
-    return (NULL);
+	system(text);
+	return (NULL);
 }
 
-void function_background_sound(char *text)
+void	ft_background_sound(char *text)
 {
-    pthread_t thread;
-    pthread_create(&thread, NULL, (void *)thread_function_handler, (text));
-    pthread_detach(thread);
-}
-void kill_sound(void)
-{
-    execve("/usr/bin/killall", (char *[]){"killall", "afplay", NULL}, NULL);
+	pthread_t	thread;
+
+	pthread_create(&thread, NULL, (void *)thread_function_handler, (text));
+	pthread_detach(thread);
 }
 
+void	kill_sound(void)
+{
+	execve("/usr/bin/killall", (char *[]){"killall", "afplay", NULL}, NULL);
+}
